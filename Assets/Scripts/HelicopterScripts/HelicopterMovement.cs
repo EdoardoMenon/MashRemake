6942e0bae952b7b2d2d6c96a7167b0d587fd775a
EdoardoMenon <edoardomenon1997@gmail.com>
2023-02-28 20:49:05 +1100

    Helicopter turns right or left depending on movement

--- Assets/Scripts/HelicopterScripts/HelicopterMovement.cs
+++ Assets/Scripts/HelicopterScripts/HelicopterMovement.cs
@@ -10,16 +10,38 @@ public class HelicopterMovement : MonoBehaviour
     private Rigidbody2D rb;
 
     private Vector2 moveDirection;
+    private bool isFacingLeft = true;
+    private Vector2 facingLeft;
+    private float horizontalInput;
+
+    private void Start()
+    {
+        facingLeft = new Vector2(-transform.localScale.x, transform.localScale.y);
+    }
 
     // Update is called once per frame
     void Update()
     {
         ProcessInputs();
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            horizontalInput = Input.GetAxis("Horizontal");
+        }
     }
 
     void FixedUpdate()
     {
         Move();
+        if (horizontalInput > 0 && isFacingLeft)
+        {
+            isFacingLeft = false;
+            Flip();
+        }
+        if (horizontalInput < 0 && !isFacingLeft)
+        {
+            isFacingLeft = true;
+            Flip();
+        }
     }
 
     void ProcessInputs()
@@ -31,6 +53,18 @@ public class HelicopterMovement : MonoBehaviour
 
     void Move()
     {
-        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed * Time.deltaTime, moveDirection.y * moveSpeed * Time.deltaTime);
+    }
+
+    void Flip()
+    {
+        if (!isFacingLeft)
+        {
+            transform.localScale = facingLeft;
+        }
+        if (isFacingLeft)
+        {
+            transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+        }
     }
 }
